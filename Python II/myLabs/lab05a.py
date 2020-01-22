65829bef830408d894f3cc3766a1986c1b3dabd7
kek <kyle@overly.dev>
2020-01-22 14:39:39 -0600

    completed lab05a.py and added to Notes.txt

---
+++ Python II/myLabs/lab05a.py
@@ -0,0 +1,30 @@
+# change this program to accept a variable number of temperatures per function call and process all of them
+# print the collector argument and its type
+# use isinstance to verify the type of each parameter as you iterate through
+# each parameter should be either int or float. reject all others. test with invalid data
+
+
+"""Temperature Conversion
+
+This program converts one temperature from fahrenheit to centigrade
+(in a void or fruitless function) and prints the results.
+Change it to handle a variable number of temperatures to covert and
+print in the function.
+"""
+
+import random
+from string import ascii_letters as abc
+
+
+def fahrenheit_to_centigrade(*args):
+    print(f"Collecter argument: {args}\t type: {type(args)}")
+    for xtmp in args:
+        if isinstance(xtmp, int) or isinstance(xtmp, float):
+            nutmp = 5.0 / 9.0 * (xtmp - 32)
+            print('{0:.1f} degrees Fahrenheit is {1:.1f} degrees Centigrade'.format(
+                xtmp, nutmp))
+
+
+temp = 72
+fahrenheit_to_centigrade(temp)
+fahrenheit_to_centigrade(72, -10.5, 'a', 111, 55)
